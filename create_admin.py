8389ac15244b58b9f0ffa4412ff02ad834c8c916
toxiccrazy91205-creator <toxiccrazy91205@gmail.com>
2026-05-11 17:07:01 +0530

    update admin script

--- create_admin.py
+++ create_admin.py
@@ -14,4 +14,7 @@ if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
     print(f"Superuser {username} created successfully.")
 else:
-    print(f"Superuser {username} already exists.")
+    u = User.objects.get(username=username)
+    u.set_password(password)
+    u.save()
+    print(f"Superuser {username} password updated.")
